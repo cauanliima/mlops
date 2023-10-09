b6ca7d77f46c609f8ce037ec20a47d9a04bf4eb8
cauanliima <cauanmarcelo@hotmail.com>
2023-10-09 16:41:10 -0300

    autopep8 correções

--- movie_recommendations/movie_recommendations.py
+++ movie_recommendations/movie_recommendations.py
@@ -1,6 +1,6 @@
+import re
 import pandas as pd
 import numpy as np
-import re
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import ipywidgets as widgets
@@ -13,6 +13,7 @@ def clean_title(title):
     title = re.sub("[^a-zA-Z0-9 ]", "", title)
     return title
 
+
 def search(title):
     title = clean_title(title)
     query_vec = vectorizer.transform([title])
@@ -22,25 +23,29 @@ def search(title):
 
     return results
 
+
 def find_similar_movies(movie_id):
-    similar_users = ratings[(ratings["movieId"] == movie_id) &\
-                             (ratings["rating"] > 4)]["userId"].unique()
-    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &\
-                                 (ratings["rating"] > 4)]["movieId"]
+    similar_users = ratings[(ratings["movieId"] == movie_id) &
+                            (ratings["rating"] > 4)]["userId"].unique()
+    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &
+                                (ratings["rating"] > 4)]["movieId"]
     similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
 
     similar_user_recs = similar_user_recs[similar_user_recs > .10]
-    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) &\
-                         (ratings["rating"] > 4)]
-    all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
+    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) &
+                        (ratings["rating"] > 4)]
+    all_user_recs = all_users["movieId"].value_counts(
+    ) / len(all_users["userId"].unique())
     rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
     rec_percentages.columns = ["similar", "all"]
-    
-    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+
+    rec_percentages["score"] = rec_percentages["similar"] / \
+        rec_percentages["all"]
     rec_percentages = rec_percentages.sort_values("score", ascending=False)
-    return rec_percentages.head(10).merge(movies, left_index=True, \
-                                            right_on="movieId")[["score", "title", "genres"]]
-    
+    return rec_percentages.head(10).merge(
+        movies, left_index=True, right_on="movieId")[["score", "title", "genres"]]
+
+
 def on_type(data):
     with recommendation_list:
         recommendation_list.clear_output()
@@ -49,7 +54,8 @@ def on_type(data):
             results = search(title)
             movie_id = results.iloc[0]["movieId"]
             display(find_similar_movies(movie_id))
-            
+
+
 movies = pd.read_csv("ml-25m/movies.csv")
 ratings = pd.read_csv("ml-25m/ratings.csv")
 
@@ -59,7 +65,7 @@ movies["clean_title"] = movies["title"].apply(clean_title)
 vectorizer = TfidfVectorizer(ngram_range=(1, 2))
 
 tfidf = vectorizer.fit_transform(movies["clean_title"])
-    
+
 movie_name_input = widgets.Text(
     value='Toy Story',
     description='Movie Title:',
